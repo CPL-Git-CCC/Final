e0edb54d67f7c371c7eb2c5740a0e6c548cc77c2
CPL-Git-CCC <lonergan01@colum.edu>
2025-04-29 19:30:06 -0500

    Update Program.cs

--- Final/Program.cs
+++ Final/Program.cs
@@ -9,10 +9,6 @@ namespace GemMiningGame
             // Create and start the game
             Game game = new Game();
             game.Run();
-
-            // Optional: Keep console open until key press
-            Console.WriteLine("\nPress any key to exit...");
-            Console.ReadKey();
         }
     }
 }
