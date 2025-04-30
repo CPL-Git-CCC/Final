7cfbcd54d6d47adfdb247d6adbe8b6956b4c4a7d
CPL-Git-CCC <lonergan01@colum.edu>
2025-04-29 19:51:51 -0500

    Update Drill.cs

--- Final/Drill.cs
+++ Final/Drill.cs
@@ -1,16 +1,18 @@
-﻿namespace GemMiningGame
+namespace GemMiningGame
 {
     public class Drill
     {
         public string Name { get; private set; }
         public int Tier { get; private set; }
         public int Cost { get; private set; }
+        public Dictionary<GemRarity, int> RarityProbabilities { get; private set; }
 
-        public Drill(string name, int tier, int cost)
+        public Drill(string name, int tier, int cost, Dictionary<GemRarity, int> rarityProbabilities)
         {
             Name = name;
             Tier = tier;
             Cost = cost;
+            RarityProbabilities = rarityProbabilities;
         }
     }
 }
