e68fed4fe75697fdabbef31c60ea2396569cdfdf
CPL-Git-CCC <lonergan01@colum.edu>
2025-04-29 19:51:27 -0500

    Update Gem.cs

--- Final/Gem.cs
+++ Final/Gem.cs
@@ -1,12 +1,21 @@
-﻿namespace GemMiningGame
+namespace GemMiningGame
 {
+    public enum GemRarity
+    {
+        Common = 1,
+        Uncommon = 2,
+        Rare = 3,
+        Epic = 4,
+        Legendary = 5
+    }
+
     public class Gem
     {
         public string Name { get; private set; }
         public int Value { get; private set; }
-        public int Rarity { get; private set; }
+        public GemRarity Rarity { get; private set; }
 
-        public Gem(string name, int value, int rarity)
+        public Gem(string name, int value, GemRarity rarity)
         {
             Name = name;
             Value = value;
