1e931e2293258372f5ae5de5919f7e7cca9827b7
CPL-Git-CCC <lonergan01@colum.edu>
2025-04-29 19:52:09 -0500

    Update Mine.cs

--- Final/Mine.cs
+++ Final/Mine.cs
@@ -1,42 +1,54 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GemMiningGame
 {
     public class Mine
     {
         private Random random;
+        private List<Gem> possibleGems;
 
         public Mine()
         {
             random = new Random();
+            possibleGems = new List<Gem>
+            {
+                new Gem("Quartz", 5, GemRarity.Common),
+                new Gem("Topaz", 15, GemRarity.Uncommon),
+                new Gem("Sapphire", 30, GemRarity.Rare),
+                new Gem("Emerald", 60, GemRarity.Epic),
+                new Gem("Diamond", 120, GemRarity.Legendary)
+            };
         }
 
         public Gem MineGem(Drill drill)
         {
-            int chance = random.Next(1, 101);
-            if (chance > 30 - (drill.Tier * 5))
-            {
+            // First determine if we find any gem at all (base 80% chance)
+            if (random.Next(1, 101) > 80 - (drill.Tier * 5))
                 return null;
+
+            // Weighted random selection based on drill's probabilities
+            var weightedGems = possibleGems
+                .Where(g => drill.RarityProbabilities.ContainsKey(g.Rarity))
+                .Select(g => new
+                {
+                    Gem = g,
+                    Weight = drill.RarityProbabilities[g.Rarity]
+                })
+                .ToList();
+
+            int totalWeight = weightedGems.Sum(w => w.Weight);
+            int randomNumber = random.Next(0, totalWeight);
+
+            foreach (var weightedGem in weightedGems)
+            {
+                if (randomNumber < weightedGem.Weight)
+                    return weightedGem.Gem;
+                randomNumber -= weightedGem.Weight;
             }
 
-            int gemType = random.Next(1, 101);
-
-            if (gemType <= 40 - (drill.Tier * 3))
-            {
-                return new Gem("Quartz", 5, 1);
-            }
-            else if (gemType <= 70 - (drill.Tier * 2))
-            {
-                return new Gem("Topaz", 15, 2);
-            }
-            else if (gemType <= 90 - drill.Tier)
-            {
-                return new Gem("Sapphire", 30, 3);
-            }
-            else
-            {
-                return new Gem("Diamond", 75, 4);
-            }
+            return null;
         }
     }
 }
